9fe7ced76e7d7ae296140be818cb0cc359d9a512
Anton Bulakh <him@necauq.ua>
2024-12-05 18:57:01 +0200

    types: rawdog a recursive return type parser that's probably broken atm

--- lua-def-parser/src/lua_parser.rs
+++ lua-def-parser/src/lua_parser.rs
@@ -1,4 +1,7 @@
-use std::str::FromStr;
+use std::{
+    fmt::{self, Display},
+    str::FromStr,
+};
 
 use anyhow::Context;
 
@@ -17,19 +20,15 @@ pub fn parse_lua(input: String) -> anyhow::Result<Vec<Result<LuaApiDef, String>>
         .next()
         .context("Lua doc input file is missing the splitter")?;
 
-    Ok(lines.map(FromStr::from_str).collect())
+    Ok(lines.map(|s| s.parse()).collect())
 }
 
 #[derive(Debug)]
 pub struct LuaApiDef {
     pub name: String,
-    pub params: Vec<String>,
-    pub ret: Option<String>,
+    pub params: Vec<LuaApiParam>,
+    pub ret: Option<LuaReturnType>,
     pub doc: Option<String>,
-}
-
-fn trim_own(s: &str) -> String {
-    s.trim().to_owned()
 }
 
 impl FromStr for LuaApiDef {
@@ -41,16 +40,25 @@ impl FromStr for LuaApiDef {
         let (name, rest) = def.split_once('(').ok_or_else(|| def.to_owned())?;
         let (params, rest) = rest.split_once(')').ok_or_else(|| def.to_owned())?;
 
-        let params = params.split(',').map(trim_own).collect();
-
+        let params = params.split(',').collect::<Vec<_>>();
         // typical split thing, eh
         let params = if params == [""] { vec![] } else { params };
 
-        let (maybe_ret, maybe_doc) = rest.split_once("[").unwrap_or((rest, ""));
-        let ret = maybe_ret.trim().strip_prefix("->").map(trim_own);
+        let params = params.into_iter().map(LuaApiParam::parse).collect();
+
+        let (maybe_ret, maybe_doc) = rest
+            .split_once("[")
+            .or_else(|| rest.split_once('(')) // "Debugist function" ones
+            .unwrap_or((rest, ""));
+
+        let ret = maybe_ret
+            .trim()
+            .strip_prefix("->")
+            .map(LuaReturnType::parse);
+
         let doc = match maybe_doc.trim() {
             "" => None,
-            doc => Some(doc.strip_suffix("]").unwrap_or(doc).to_owned()),
+            doc => Some(doc.strip_suffix("]").unwrap_or(doc).replace("\\n", "\n")),
         };
 
         Ok(Self {
@@ -59,5 +67,171 @@ impl FromStr for LuaApiDef {
             ret,
             doc,
         })
+    }
+}
+
+#[derive(Debug)]
+pub struct LuaApiParam {
+    pub name: String,
+    pub ty: String,
+    pub default: Option<String>,
+}
+
+impl LuaApiParam {
+    fn parse(s: &str) -> Self {
+        let s = s.trim();
+        let (name, rest) = s.split_once(':').unwrap_or((s, ""));
+        let (ty, default) = rest.split_once('=').unwrap_or((rest, ""));
+
+        Self {
+            name: name.trim().to_owned(),
+            ty: ty.trim().to_owned(),
+            default: match default.trim() {
+                "" => None,
+                _ => Some(default.to_owned()),
+            },
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum LuaReturnType {
+    Basic(String),
+    BasicNamed(String, String),
+    Array(Box<LuaReturnType>),
+    Multi(Vec<Box<LuaReturnType>>),
+    Disjoint(Vec<Box<LuaReturnType>>),
+}
+
+impl LuaReturnType {
+    pub fn parse(s: &str) -> Self {
+        let s = s.trim();
+        let s = s
+            .strip_prefix('(')
+            .and_then(|s| s.strip_suffix(')'))
+            .unwrap_or(s);
+
+        if let Some(inner) = s.strip_prefix('{').and_then(|s| s.strip_suffix('}')) {
+            return Self::Array(Box::new(Self::parse(inner)));
+        }
+
+        let mut multi_parts = vec![];
+        let mut disjoint_parts = vec![];
+        let mut current = String::new();
+        let mut parens_depth = 0;
+
+        for c in s.chars() {
+            match c {
+                '(' => {
+                    parens_depth += 1;
+                    current.push(c);
+                }
+                ')' => {
+                    parens_depth -= 1;
+                    current.push(c);
+                }
+                ',' if parens_depth == 0 => {
+                    multi_parts.push(current.trim().to_owned());
+                    current.clear();
+                }
+                '|' if parens_depth == 0 => {
+                    disjoint_parts.push(current.trim().to_owned());
+                    current.clear();
+                }
+                _ => current.push(c),
+            }
+        }
+
+        if !current.is_empty() {
+            if !multi_parts.is_empty() {
+                multi_parts.push(current.trim().to_owned());
+            } else {
+                disjoint_parts.push(current.trim().to_owned());
+            }
+        }
+
+        if multi_parts.len() > 1 {
+            let parsed_parts = multi_parts
+                .into_iter()
+                .map(|part| Box::new(Self::parse(&part)))
+                .collect();
+            return Self::Multi(parsed_parts);
+        }
+
+        if disjoint_parts.len() > 1 {
+            let parsed_parts = disjoint_parts
+                .into_iter()
+                .map(|part| Box::new(Self::parse(&part)))
+                .collect();
+            return Self::Disjoint(parsed_parts);
+        }
+
+        if let Some((name, basic)) = s.split_once(':') {
+            return Self::BasicNamed(name.trim().to_owned(), basic.trim().to_owned());
+        }
+
+        Self::Basic(s.to_owned())
+    }
+}
+
+impl Display for LuaReturnType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use LuaReturnType::*;
+        match self {
+            Basic(s) => write!(f, "{s}"),
+            BasicNamed(name, ty) => write!(f, "{name}:{ty}"),
+            Array(inner) => write!(f, "{{{inner}}}"),
+            Multi(parts) => {
+                write!(f, "(")?;
+                for (i, part) in parts.iter().enumerate() {
+                    write!(f, "{part}")?;
+                    if i != parts.len() - 1 {
+                        write!(f, ",")?;
+                    }
+                }
+                write!(f, ")")?;
+                Ok(())
+            }
+            Disjoint(parts) => {
+                write!(f, "(")?;
+                for (i, part) in parts.iter().enumerate() {
+                    write!(f, "{part}")?;
+                    if i != parts.len() - 1 {
+                        write!(f, "|")?;
+                    }
+                }
+                write!(f, ")")?;
+                Ok(())
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_return_types() {
+        fn check(s: &str, expected: &str) {
+            println!("Checking: {s}");
+            let parsed = LuaReturnType::parse(s);
+            assert_eq!(format!("{parsed}"), expected);
+        }
+
+        check("component", "component");
+        check("entity_scale:number", "entity_scale:number");
+        check("{number}", "{number}");
+        check("number,component", "(number,component)");
+        check("number|component", "(number|component)");
+        check("{number,component}", "{(number,component)}");
+        check(
+            "(number|component),entity_scale:number",
+            "((number|component),entity_scale:number)",
+        );
+        check(
+            "number|(component,entity_scale:number)",
+            "(number|(component,entity_scale:number))",
+        );
     }
 }
