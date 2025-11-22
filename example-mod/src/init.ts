865ea5765d6e81fad6a812ec183693f5d074135c
Anton Bulakh <him@necauq.ua>
2025-11-22 07:04:18 +0200

    Update the example mod

--- example-mod/src/init.ts
+++ example-mod/src/init.ts
@@ -1,15 +1,15 @@
-import { events } from "@noita-ts/base";
+import noita from "@noita-ts/base";
 
-events.on("PlayerSpawned", (player_entity) => {
+noita.on("PlayerSpawned", (player_entity) => {
   GamePrint(`player spawned: ${player_entity}`);
 });
 
-events.on("PausedChanged", (is_paused, is_inventory_pause) => {
+noita.on("PausedChanged", (is_paused, is_inventory_pause) => {
   GamePrint(
     `paused changed: is_paused: ${is_paused}, inventory pause: ${is_inventory_pause}`,
   );
 });
 
-events.on("PlayerDied", (player_entity) => {
+noita.on("PlayerDied", (player_entity) => {
   GamePrint(`player died: ${player_entity}`);
 });
