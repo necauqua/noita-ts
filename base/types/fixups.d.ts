27eaff19085d1146ae6338b64132074d6aaf5a4f
Anton Bulakh <him@necauq.ua>
2026-03-04 04:56:27 +0200

    Add more api declaration fixups

--- base/types/fixups.d.ts
+++ base/types/fixups.d.ts
@@ -1,5 +1,94 @@
 /** @noSelfInFile */
 
+declare function EntityAddChild(parent_id: EntityID, child_id: EntityID): void;
+
+/**
+ * A string union of component field names for given component id `C`
+ * (if it's a Component<"ComponentName">, otherwise its just string),
+ * that is also filtered by whether the fields extend `F`.
+ */
+type ComponentField<C extends ComponentID, F = any> =
+  C extends Component<infer N>
+    ? keyof {
+        [K in keyof ComponentShapes[N] as ComponentShapes[N][K] extends F
+          ? K
+          : never]: any;
+      }
+    : string;
+
+type ComponentValue<C extends ComponentID, K> =
+  C extends Component<infer N>
+    ? K extends keyof ComponentShapes[N]
+      ? ComponentShapes[N][K]
+      : unknown
+    : unknown;
+
+/** @deprecated Use EntityAddComponent2 instead */
+declare function EntityAddComponent<N extends ComponentName>(
+  entity_id: EntityID,
+  component_type_name: N,
+  values?: (keyof ComponentShapes[N] | (string & {}))[],
+): ComponentID;
+
+declare function EntityAddComponent2<N extends ComponentName>(
+  entity_id: EntityID,
+  component_type_name: N,
+  values: Partial<ComponentShapes[N]> & { _enabled?: boolean; _tags?: string },
+): ComponentID;
+
+/** @deprecated Use ComponentGetValue2 instead */
+declare function ComponentGetValue<
+  N extends ComponentName,
+  C extends ComponentID | Component<N>,
+>(component: C, variable_name: ComponentField<C, string>): string | undefined;
+
+/** @deprecated Use ComponentGetValue2 instead */
+declare function ComponentGetValueBool<
+  N extends ComponentName,
+  C extends ComponentID | Component<N>,
+>(component: C, variable_name: ComponentField<C, boolean>): boolean | undefined;
+
+/** @deprecated Use ComponentGetValue2 instead */
+declare function ComponentGetValueInt<
+  N extends ComponentName,
+  C extends ComponentID | Component<N>,
+>(component: C, variable_name: ComponentField<C, number>): number | undefined;
+
+/** @deprecated Use ComponentGetValue2 instead */
+declare function ComponentGetValueFloat<
+  N extends ComponentName,
+  C extends ComponentID | Component<N>,
+>(component: C, variable_name: ComponentField<C, number>): number | undefined;
+
+/** @deprecated Use ComponentGetValue2 instead */
+declare function ComponentGetValueVector2<
+  N extends ComponentName,
+  C extends ComponentID | Component<N>,
+>(
+  component: C,
+  variable_name: ComponentField<C, [number, number]>,
+): LuaMultiReturn<[number, number]> | LuaMultiReturn<[undefined | undefined]>;
+
+declare function ComponentGetValue2<
+  N extends ComponentName,
+  C extends ComponentID | Component<N>,
+  V extends ComponentField<C>,
+>(component: C, variable_name: V): ComponentValue<C, V>;
+
+/** @deprecated Use ComponentSetValue2 instead */
+declare function ComponentSetValue<
+  N extends ComponentName,
+  C extends ComponentID | Component<N>,
+>(component: C, variable_name: ComponentField<C>, value: string): void;
+
+declare function ComponentSetValue2<
+  N extends ComponentName,
+  C extends ComponentID | Component<N>,
+  V extends ComponentField<C>,
+>(component: C, field_name: V, value: ComponentValue<C, V>): void;
+
+declare function ComponentGetTypeName(component_id: ComponentID): ComponentName;
+
 type VectorItems = {
   int: number;
   float: number;
@@ -7,8 +96,6 @@ type VectorItems = {
 };
 
 type VectorType = keyof VectorItems;
-
-declare function ComponentGetTypeName(component_id: ComponentID): ComponentName;
 
 declare function ComponentGetVectorSize(
   component_id: ComponentID,
@@ -29,24 +116,28 @@ declare function ComponentGetVector<T extends VectorType>(
   type_stored_in_vector: T,
 ): VectorItems[T][] | undefined;
 
+declare function EntityGetComponent<N extends ComponentName>(
+  entity_id: EntityID,
+  component_type_name: N,
+  tag?: string,
+): Component<N>[] | undefined;
+
+declare function EntityGetComponentIncludingDisabled<N extends ComponentName>(
+  entity_id: EntityID,
+  component_type_name: N,
+  tag?: string,
+): Component<N>[] | undefined;
+
 declare function EntityGetFirstComponent<N extends ComponentName>(
   entity_id: EntityID,
   component_type_name: N,
   tag?: string,
 ): Component<N> | undefined;
 
-type ComponentKey<C extends ComponentID> =
-  C extends Component<infer N> ? keyof ComponentShapes[N] : string;
-
-type ComponentValue<C extends ComponentID, K> =
-  C extends Component<infer N>
-    ? K extends keyof ComponentShapes[N]
-      ? ComponentShapes[N][K]
-      : unknown
-    : unknown;
-
-declare function ComponentGetValue2<
+declare function EntityGetFirstComponentIncludingDisabled<
   N extends ComponentName,
-  C extends ComponentID | Component<N>,
-  V extends ComponentKey<C>,
->(component_id: C, variable_name: V): ComponentValue<C, V>;
+>(
+  entity_id: EntityID,
+  component_type_name: N,
+  tag?: string,
+): Component<N> | undefined;
